a1543b57b69ba9a65a4eae2c4591fefc99322f19
Kunal Parkhade <kunalparkhade@gmail.com>
2024-11-22 12:55:40 +0530

    Error resolved

--- app/firebase_config.py
+++ app/firebase_config.py
@@ -1,11 +1,13 @@
+import json
 import firebase_admin
 from firebase_admin import credentials, firestore
+import streamlit as st
 
-# Path to your Firebase credentials
-CREDENTIALS_PATH = "firebase_credentials.json"
+# # Path to your Firebase credentials
+# CREDENTIALS_PATH = "firebase_credentials.json"
 
 # Initialize Firebase app
-cred = credentials.Certificate(CREDENTIALS_PATH)
+cred = credentials.Certificate("firebase_credentials.json")
 firebase_admin.initialize_app(cred)
 
 # Initialize firestore
