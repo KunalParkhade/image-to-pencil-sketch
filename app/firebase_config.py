28fc0421a3fe722461358222d5825621aedc5ffa
Kunal Parkhade <kunalparkhade@gmail.com>
2024-11-22 13:09:53 +0530

    Error resolved during deployment

--- app/firebase_config.py
+++ app/firebase_config.py
@@ -6,8 +6,10 @@ import streamlit as st
 # # Path to your Firebase credentials
 # CREDENTIALS_PATH = "firebase_credentials.json"
 
+firebase_credentials = json.loads(st.secrets["firebase"]["credentials"])
+
 # Initialize Firebase app
-cred = credentials.Certificate("firebase_credentials.json")
+cred = credentials.Certificate(firebase_credentials)
 firebase_admin.initialize_app(cred)
 
 # Initialize firestore
